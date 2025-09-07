8f20054e71cc29d360094ce26751dd58f5507d26
Uriel Velazquez <evelazqueza1900@alumno.ipn.mx>
2025-09-07 13:44:28 -0600

    V1.6
    Now the A and U is available, a new image is available

--- practica1/imagen.py
+++ practica1/imagen.py
@@ -124,7 +124,7 @@ def pintar_h(imagen, height, width):
 	region = region.astype(np.float32)
 
 
-	#funcion para aplicar el color, verde, azul rojo a la imagen (manera vertical)
+	#funcion para aplicar el color, verde, azul rojo en forma de H a la imagen
 def dibujar_H(imagen, height, width):
 	
 	intensidad = .7
@@ -214,21 +214,191 @@ def dibujar_H(imagen, height, width):
 
 
 
+#funcion para aplicar el color, verde, azul rojo en forma de U a la imagen
+def dibujarU(imagen, height, width):
+	
+	intensidad = .7
+
+	matriz_img = np.array(imagen)
+
+	img_filtrada = matriz_img.copy()
+
+
+	div_width = width //5
+
+	div_height = height //11
+	
+	
+	#para la primer parte de la imagen(rojo)
+
+	region = img_filtrada[0: height, 0 : div_width  ]
+	region = region.astype(np.float32)
+
+
+	region = np.clip(region,0,255)
+
+	region[...,1] = (1- intensidad)
+	region[...,2] = (1- intensidad)
+
+	img_filtrada[0:height, 0:div_width ] = region.astype(np.uint8)
+
+
+
+	#para la primer parte de la imagen(rojo)
+
+	region = img_filtrada[0: height, width - div_width   : width  ]
+	region = region.astype(np.float32)
+
+
+	region = np.clip(region,0,255)
+
+	region[...,1] = (1- intensidad)
+	region[...,0] = (1- intensidad)
+
+	img_filtrada[0:height, width - div_width  : width] = region.astype(np.uint8)
+
+
+	#para el segundo | con color verde
+
+	region = img_filtrada[0: height, div_width : div_width * 2 ]
+	region = region.astype(np.float32)
+
+
+	region = np.clip(region,0,255)
+
+	region[...,0] = (1- intensidad)
+	region[...,2] = (1- intensidad)
+
+	img_filtrada[0:height, div_width:div_width * 2] = region.astype(np.uint8)
+
+
+	#para el segundo | con color verde
+
+	region = img_filtrada[0: height, div_width + (2 * div_width ): width - div_width    ]
+	region = region.astype(np.float32)
+
+
+	region = np.clip(region,0,255)
+
+	region[...,0] = (1- intensidad)
+	region[...,2] = (1- intensidad)
+
+	img_filtrada[0:height,div_width + (2 * div_width ) : width - div_width ] = region.astype(np.uint8)
+
+
+	#para el - con color verde
+
+	region = img_filtrada[div_height *8: height, div_width * 2: div_width + (2 * div_width ) ]
+	region = region.astype(np.float32)
+
+
+	region = np.clip(region,0,255)
+
+	region[...,0] = (1- intensidad)
+	region[...,2] = (1- intensidad)
+
+	img_filtrada[div_height * 8:height,div_width * 2: div_width + (2 * div_width ) ] = region.astype(np.uint8)
+
+
+	return Image.fromarray(img_filtrada)
+
+
+ 
+#funcion para aplicar el color, verde, azul rojo en forma de U a la imagen
+def dibujarA(imagen, height, width):
+	intensidad = .7
+	matriz_img = np.array(imagen)
+	img_filtrada = matriz_img.copy()
+	div_width = width //24
+	div_height = height //11
+	
+	#para la primer parte de la imagen(rojo)
+
+	region = img_filtrada[0: height, 0 : div_width * 8]
+	region = region.astype(np.float32)
+	region = np.clip(region,0,255)
+	region[...,1] = (1- intensidad)
+	region[...,2] = (1- intensidad)
+	img_filtrada[0:height, 0:div_width * 8] = region.astype(np.uint8)
+
+	#para el | de la A 
+
+	region = img_filtrada[0:height, div_width * 8 : div_width*10]
+	region = region.astype(np.float32)
+	region = np.clip(region,0,255)
+	region[...,0] = (1 - intensidad)
+	region[...,2] = (1 - intensidad)
+
+	img_filtrada[0:height, div_width* 8: div_width*10] = region.astype(np.uint8)
+
+
+	#para el centro de la A
+	region = img_filtrada[0 : div_height*2, div_width*10: div_width*14]
+	region = region.astype(np.float32)
+	region = np.clip(region,0,255)
+	region[...,0] = (1 - intensidad)
+	region[...,2] = (1 - intensidad)
+
+	img_filtrada[0:div_height*2, div_width*10:div_width*14] = region.astype(np.uint8)
+
+
+	#para el centro de la A
+	region = img_filtrada[div_height*4 : div_height*6 , div_width*10: div_width*14]
+	region = region.astype(np.float32)
+	region = np.clip(region,0,255)
+	region[...,0] = (1 - intensidad)
+	region[...,2] = (1 - intensidad)
+
+	img_filtrada[div_height*4 :div_height*6, div_width*10:div_width*14] = region.astype(np.uint8)
+
+
+	#para el | de la A
+
+	region = img_filtrada[0: height, div_width*14:div_width*16] 
+	region = region.astype(np.float32)
+	region = np.clip(region,0,255)
+	region[...,0] = (1 - intensidad)
+	region[...,2] = (1 - intensidad)
+
+	img_filtrada[0:height, div_width*14: div_width*16] = region.astype(np.uint8)
+
+	#para la ultima parte de color azul
+
+	region = img_filtrada[0:height, div_width * 16: width]
+	region = region.astype(np.float32)
+	region = np.clip(region,0,255)
+	region[...,0] = (1 - intensidad)
+	region[...,1] = (1 - intensidad)
+	img_filtrada[0:height, div_width * 16: width] = region.astype(np.uint8)
+
+
+
+	return Image.fromarray(img_filtrada)
+
+
+def dibujar_U(imagen,height,width):
+	intensidad = .7
+	matriz_img = np.array(imagen)
+	img_filtrada = matriz_img.copy()
+	div_width = width //5
+	div_height = height //11
+	
+
 def menu_imagenes():
 	print("\n\n\n1.Imagen de Berserk")
 	print("2.Imagen de evangelon")
 	print("3.Imagen de evangelon(variante)")
-	print("4.Imagen de Berserk")
+	print("4.Imagen de Berserk(variante)")
 	print("5.Imagen de perfect blue")
+	print("6.Imagen de The bends")
 	return input("Selecciona una opcion para poder colorearla(-1 para poder salir): ")
 
 
-
-
 def menu():
-	print("\n\n\n\n1.Colorear la imagen de manera vertical.")
-	print("2.Colorear la imagen de manera horizontal.")
-	print("3.Colorear la inicial de los miembros del equipo (H,U,A).")
+	print("\n\n\n\n1.Mostrar la imagen original.")
+	print("2.Colorear la imagen de manera vertical")
+	print("3.Colorear la imagen de manera horizontal.")
+	print("4.Colorear la inicial de los miembros del equipo (H,U,A).")
 	print("-1 Para cambiar de imagen")
 	return input("Ingresa una opcion para poder continuar: ")
 
@@ -236,20 +406,33 @@ def menu():
 def seleccion(im, op):
 	width, height = im.size
 	if op == '1':
+		print("Imagen original.\n\n\n")
+		im.show()
+	if op == '2':
 		print("\n\n\nColoreando imagen de manera vertical\n\n\n")
 		new_im = color_vertical(im , height, width)
 		new_im.show()
-	elif op =='2':
+	elif op =='3':
 		print("\n\n\nColoreando imagen de manera horizontal\n\n\n")
 		new_im = color_horizontal(im,height,width)
 		new_im.show()
-	elif op =='3':
+	elif op =='4':
 		caso = True
 		while caso:
 			inicial = input("Ingresa la vocal para colorear (H,U,A): ").upper()
 			if inicial == 'H':
-				print("Se imprimira la inicial de Hector (H)")
+				print("Se imprimira la inicial de Hector (H).\n\n\n")
 				new_im = dibujar_H(im,height,width)
+				new_im.show()
+				caso = False
+			if inicial == 'U':
+				print("Se imprimira la inicial de Uriel (U).\n\n\n")
+				new_im = dibujarU(im,height,width)
+				new_im.show()
+				caso = False
+			if inicial == 'A':
+				print("Se imprimira la inicial de Alejandro (A).\n\n\n")
+				new_im = dibujarA(im,height,width)
 				new_im.show()
 				caso = False
 			else:
@@ -305,5 +488,13 @@ if __name__ == "__main__":
 					break
 				else:
 					seleccion(im,op1)
+		elif sel == '6':
+			im = Image.open('img6.jpeg')
+			while op1 != '-1':
+				op1 = menu()
+				if op1 == '-1':
+					break
+				seleccion(im,op1)
 		else:
 			print("Ingresa una opcion valida para poder continuar (1-5) \n\n\n")
+	print("Programa finalizado")
