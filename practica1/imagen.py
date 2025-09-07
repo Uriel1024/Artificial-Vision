bbaa514a57494d15074616f0e1c18e7ceec10a56
Uriel Velazquez <evelazqueza1900@alumno.ipn.mx>
2025-09-06 23:51:50 -0600

    V1.5
    The menu now works, only left the letter U and A

--- practica1/imagen.py
+++ practica1/imagen.py
@@ -214,65 +214,96 @@ def dibujar_H(imagen, height, width):
 
 
 
+def menu_imagenes():
+	print("\n\n\n1.Imagen de Berserk")
+	print("2.Imagen de evangelon")
+	print("3.Imagen de evangelon(variante)")
+	print("4.Imagen de Berserk")
+	print("5.Imagen de perfect blue")
+	return input("Selecciona una opcion para poder colorearla(-1 para poder salir): ")
+
+
+
+
 def menu():
 	print("\n\n\n\n1.Colorear la imagen de manera vertical.")
 	print("2.Colorear la imagen de manera horizontal.")
 	print("3.Colorear la inicial de los miembros del equipo (H,U,A).")
-	print("-1 Para salir del programa")
+	print("-1 Para cambiar de imagen")
 	return input("Ingresa una opcion para poder continuar: ")
 
 
-def leer_img():
-	asdf = input("\n\n\nDeseas seguir ejecutando el programa(ingresa 'salir', '-1', exit para 'salir'):")
-	if asdf in ['salir', '-1', 'exit']:
-		print("Saliendo del programa.\n\n\n")
-		return "-1"
+def seleccion(im, op):
+	width, height = im.size
+	if op == '1':
+		print("\n\n\nColoreando imagen de manera vertical\n\n\n")
+		new_im = color_vertical(im , height, width)
+		new_im.show()
+	elif op =='2':
+		print("\n\n\nColoreando imagen de manera horizontal\n\n\n")
+		new_im = color_horizontal(im,height,width)
+		new_im.show()
+	elif op =='3':
+		caso = True
+		while caso:
+			inicial = input("Ingresa la vocal para colorear (H,U,A): ").upper()
+			if inicial == 'H':
+				print("Se imprimira la inicial de Hector (H)")
+				new_im = dibujar_H(im,height,width)
+				new_im.show()
+				caso = False
+			else:
+				print(f"{inicial} No es una opcion valida, ingresa una  letra valida(H,U,A).\n\n\n")
+				caso = True
 	else:
-		return "asdfas"
-
-def agregar_imagen():
-	imag = input("Ingresa el nombre del archivo e imagen con su extension para colrearla: ")
-	try:
-		imagenes = Image.open(imag)
-		return imagenes
-	except FileNotFoundError:
-		print(f"Error, archivo no encontrado, {menu} \n\n\n")
-	except Exception as e:
-		print(f"Ocurrio un error {e}  \n\n\n")
-
-
+		print("Ingresa una opcon valida (1-3). \n\n\n")
 
 if __name__ == "__main__":
-	op = 'asfd'
-	while op != '-1':
-		op = leer_img()
-		if op == "-1":
-			break
-		im = agregar_imagen()
-		width, height = im.size
-		op = menu()
-		if op == '1':
-			print("\n\n\nColoreando imagen de manera vertical\n\n\n")
-			new_im = color_vertical(im , height, width)
-			new_im.show()
-
-		elif op == '2':
-			print("\n\n\nColoreando imagen de manera horizontal\n\n\n")
-			new_im = color_horizontal(im,height,width)
-			new_im.show()
-		elif op == '3':
-			caso = True
-			while caso:
-				inicial = input("Ingresa la vocal para colorear (H,U,A): ").upper()
-
-				if inicial == 'H':
-					print("Se imprimira la inicial de Hector (H)")
-					new_im = dibujar_H(im,height,width)
-					new_im.show()
-					caso = False
+	print("Bienvenido a la primer practica de Vision Artificial.")
+	print("Selecciona una imagen para poder continuar.\n\n\n\n")
+	sel = ""
+	while sel != "-1":
+		op1 = ''
+		sel = menu_imagenes()
+		if sel == '1':
+			im = Image.open('img1.jpeg')
+			while op1 != '-1':
+				op1 = menu()
+				if op1 == '-1':
+					break
 				else:
-					print(f"{inicial} No es una opcion valida, ingresa una  letra valida(H,U,A).\n\n\n")
-					caso = True
-
-
-	print("\n\n\nPrograma finalizado\n\n\n")
+					seleccion(im,op1)
+		elif sel == '2':
+			im = Image.open('img2.jpeg')
+			while op1 != '-1':
+				op1 = menu()
+				if op1 == '-1':
+					break
+				else:
+					seleccion(im,op1)
+		elif sel == '3':
+			im = Image.open('img3.jpeg')
+			while op1 != '-1':
+				op1 = menu()
+				if op1 == '-1':
+					break
+				else:
+					seleccion(im,op1)
+		elif sel == '4':
+			im = Image.open('img4.jpg')
+			while op1 != '-1':
+				op1 = menu()
+				if op1 == '-1':
+					break
+				else:
+					seleccion(im,op1)
+		elif sel == '5':
+			im = Image.open('img5.jpeg')
+			while op1 != '-1':
+				op1 = menu()
+				if op1 == '-1':
+					break
+				else:
+					seleccion(im,op1)
+		else:
+			print("Ingresa una opcion valida para poder continuar (1-5) \n\n\n")
